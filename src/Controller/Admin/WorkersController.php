5ffdc5d0d065984a6a6f7662f26b68be51e73886
Damian Bielecki <damianb121@gmail.com>
2021-11-23 10:28:00 +0100

    dodawanie stanowiska do bazy

--- src/Controller/Admin/WorkersController.php
+++ src/Controller/Admin/WorkersController.php
@@ -5,22 +5,58 @@ namespace App\Controller\Admin;
 use App\Controller\Admin\Core\BaseAdminController;
 use App\Entity\Worker;
 use App\Entity\WorkPlace;
-use App\Repository\WorkerRepository;
+use App\Form\NewWorkPlaceType;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 
 class WorkersController extends BaseAdminController
 {
-    public function index($id): Response
+    private $_sess = null;
+    public function __construct(SessionInterface $sess)
     {
+        $this->_sess = $sess;
+    }
+
+    public function index($id, Request $request): Response
+    {
+        $wp = new WorkPlace();
+        $form = $this->createForm(NewWorkPlaceType::class, $wp);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $wp = $form->getData();
+            $mng = $this->getDoctrine()->getManager();
+
+            $mng->persist($wp);
+            $mng->flush();
+
+            $this->addFlash('msg', sprintf("Utworzono nowe stanowisko  %s (id: %d) ", $wp->getName(), $wp->getID()));
+            $this->addFlash('successForm', 1);
+
+            return $this->redirect($request->getUri());
+        }
+        else if($form->isSubmitted())
+        {
+            $this->addFlash('successForm', 0);
+            $this->addFlash('msg', "Blad podczas dodawania danych");
+        }
+
         $workers = $this->getDoctrine()
             ->getRepository(Worker::class)
             ->findAll();
 
+        $workPlaces = $this->getDoctrine()->getRepository(WorkPlace::class)
+            ->findAll();
 
         return $this->render('admin/workers/index.html.twig', [
-            'active_nav_route' => 'admin_workers',
-            'content_title' => 'Lista wszystkich pracowników',
-            'workers' => $workers,
+            'active_nav_route'  => 'admin_workers',
+            'content_title'     => 'Lista wszystkich pracowników',
+            'workers'           => $workers,
+            'workPlaces'        => $workPlaces,
+            'newWorkPlaceForm'  => $form->createView(),
         ]);
     }
     public function add(): Response
--- src/Entity/WorkPlace.php
+++ src/Entity/WorkPlace.php
@@ -6,9 +6,12 @@ use App\Repository\WorkPlaceRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=WorkPlaceRepository::class)
+ * @UniqueEntity("name", message="{{ value }} już istnieje!")
  */
 class WorkPlace
 {
@@ -21,11 +24,17 @@ class WorkPlace
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(
+     *      min = 4,
+     *      max = 255,
+     *      minMessage = "Nazwa musi mieć więcej niż {{ limit }} znaki",
+     *      maxMessage = "Nazwa musi być krótsza niż {{ limit }} znaków")
      */
     private $name;
 
     /**
      * @ORM\Column(type="float")
+     * Assert\PositiveOrZero(message="Zarobki muszą być większe lub równe 0")
      */
     private $min_earnings;
 
@@ -33,6 +42,12 @@ class WorkPlace
      * @ORM\OneToMany(targetEntity=Worker::class, mappedBy="workPlace")
      */
     private $workers;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Assert\NotBlank(message="Opis nie może być pusty")
+     */
+    private $description;
 
     public function __construct()
     {
@@ -95,6 +110,18 @@ class WorkPlace
                 $worker->setWorkPlace(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
 
         return $this;
     }
